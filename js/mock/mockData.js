d33f6521f8ddf8661f3025f874e4c51ee0c5bddc
jordi <jordi.lanau@yahoo.com>
2021-12-01 11:42:32 +0000

    Render jobs search mock data #9

--- js/mock/mockData.js
+++ js/mock/mockData.js
@@ -2,14 +2,14 @@ let mockJob = {
   title: 'job title',
   employer: 'employer name',
   location: 'location',
-  minSalary: 'min salary',
-  maxSalary: 'max salary',
+  minSalary: '20000',
+  maxSalary: '30000',
   description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi, tenetur.',
 };
 
 const numberOfJobs = 10;
 
-const jobs = [...Array(numberOfJobs)].map((job, index) => {
+export const jobs = [...Array(numberOfJobs)].map((job, index) => {
   job = Object.assign({}, mockJob);
   job.title = `job #${index + 1}`;
   return job;
