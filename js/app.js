5a47ee1aa20b62dec5ffd94972a9e7719c08cc04
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-12-01 14:38:47 +0000

    adds event listeners to all inputs and renders boolean object for checks upon submit

--- js/app.js
+++ js/app.js
@@ -1,31 +1,45 @@
 import "./utils/jobsSearch.js";
 
-const keywordsElement = document.querySelector("input.what"); // Define DOM elements
+// Define DOM elements
+const keywordsElement = document.querySelector("input.what");
 const locationElement = document.querySelector("input.where");
 const submitElement = document.querySelector("button.submit");
 const distanceElement = document.querySelector("select.distance");
 const salaryFromElement = document.querySelector("select.starting-salary");
 const salaryToElement = document.querySelector("select.ending-salary");
 
-console.log(keywordsElement);
-console.log(locationElement);
-console.log(submitElement);
-console.log(distanceElement);
-console.log(salaryFromElement);
-console.log(salaryToElement);
+// Select all checkboxes with the name 'settings' using querySelectorAll.
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+
+let checkedCriteria = [];
+
+// Use Array.forEach to add an event listener to each checkbox.
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {
+    checkedCriteria = [...checkboxes]
+      .filter((i) => i.checked) // Use Array.filter to remove unchecked checkboxes.
+      .map((i) => i.attributes[1].nodeValue); // Use Array.map to extract only the checkbox names from the array of objects.
+
+    console.log(checkedCriteria);
+  });
+});
 
 const submitFunction = function (event) {
   event.preventDefault();
   let locationName = locationElement.value;
   let keywords = keywordsElement.value;
   let distance = distanceElement.value;
-  let salaryFrom = salaryFromElement.value;
-  let salaryTo = salaryToElement.value;
+  let minimumSalary = salaryFromElement.value;
+  let maximumSalary = salaryToElement.value;
+  let checkedObj = Object.assign(
+    ...checkedCriteria.map((k) => ({ [k]: true }))
+  );
   console.log(locationName);
   console.log(keywords);
   console.log(distance);
-  console.log(salaryFrom);
-  console.log(salaryTo);
+  console.log(minimumSalary);
+  console.log(maximumSalary);
+  console.log(checkedObj);
 };
 
 submitElement.addEventListener("click", submitFunction);
