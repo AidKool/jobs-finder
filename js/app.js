2093ab10dda48cc02421d979864aa920f0b5c375
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-12-01 12:11:49 +0000

    pulls values from what, where, distance, salaryFrom, salaryTo upon submit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
-import './utils/jobsSearch.js';
+import "./utils/jobsSearch.js";
+
+const keywordsElement = document.querySelector("input.what"); // Define DOM elements
+const locationElement = document.querySelector("input.where");
+const submitElement = document.querySelector("button.submit");
+const distanceElement = document.querySelector("select.distance");
+const salaryFromElement = document.querySelector("select.starting-salary");
+const salaryToElement = document.querySelector("select.ending-salary");
+
+console.log(keywordsElement);
+console.log(locationElement);
+console.log(submitElement);
+console.log(distanceElement);
+console.log(salaryFromElement);
+console.log(salaryToElement);
+
+const submitFunction = function (event) {
+  event.preventDefault();
+  let locationName = locationElement.value;
+  let keywords = keywordsElement.value;
+  let distance = distanceElement.value;
+  let salaryFrom = salaryFromElement.value;
+  let salaryTo = salaryToElement.value;
+  console.log(locationName);
+  console.log(keywords);
+  console.log(distance);
+  console.log(salaryFrom);
+  console.log(salaryTo);
+};
+
+submitElement.addEventListener("click", submitFunction);
