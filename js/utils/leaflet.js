ca5a35f5e8020476f54faf4a9d00c9f13de46ee2
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-12-02 10:40:44 +0000

    renders map with markers in new section element

--- js/utils/leaflet.js
+++ js/utils/leaflet.js
@@ -1,8 +1,7 @@
 // var mapElement = document.getElementById(map);
+export const map = L.map("map").setView([53.483959, -2.244644], 6);
 
-var map = L.map("map").setView([53.483959, -2.244644], 6);
-
-L.tileLayer(
+export const tileLayer = L.tileLayer(
   "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=asHo0upHjsCi8cGPV9nd",
   {
     attribution:
@@ -10,5 +9,5 @@ L.tileLayer(
   }
 ).addTo(map);
 
-var marker = L.marker([53.483959, -2.244644]).addTo(map);
-var marker = L.marker([51.5, -0.09]).addTo(map);
+export let marker_man = L.marker([53.483959, -2.244644]).addTo(map);
+export let marker_ldn = L.marker([51.5, -0.09]).addTo(map);
