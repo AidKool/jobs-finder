7f2b85670eca2eb10fb80f0e3147b2fe5d68b004
jordi <jordi.lanau@yahoo.com>
2021-12-02 16:23:26 +0000

    Add comments to the pagination module

--- js/utils/pagination.js
+++ js/utils/pagination.js
@@ -1,3 +1,12 @@
+// the function below does not work
+
+/*
+1. it was just moved from a different context
+where it had access to all parameters
+2. the result of fetchJobs() must be stored in
+in a variable
+*/
+
 export function paginate(event) {
   if (event.target.tagName === 'A') {
     let page = event.target.dataset.page;
@@ -10,7 +19,6 @@ export function paginate(event) {
       currentPage = page;
     }
     resultsToSkip = (currentPage - 1) * resultsToTake;
-    console.log(currentPage);
     fetchJobs();
   }
 }
