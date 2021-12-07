3b60cf859a4fb4eaf30e09c8987594b6a719d530
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-12-07 11:06:38 +0000

    call ons fetch only upon initial load or if not locally stored

--- js/app.js
+++ js/app.js
@@ -113,34 +113,38 @@ window.addEventListener('DOMContentLoaded', () => {
     coordinates.push(coords);
   });
 
-  factors.forEach(async (factor) => {
-    try {
-      const url = renderOnsUrl(factor);
-      const data = await fetchOnsData(url);
-      const { observations } = data;
-      let wellbeing = observations.map(({ observation }) => observation);
-      let geography = observations.map((a) => a.dimensions['Geography'].label);
-      let result = {};
-      geography.forEach((key, i) => (result[key] = wellbeing[i]));
-      const filtered = Object.keys(result)
-        .filter((key) => cities.includes(key))
-        .reduce((obj, key) => {
-          obj[key] = result[key];
-          return obj;
-        }, {});
-      factorsFiltered.push(filtered);
-      // Object created
-      let obj = {};
+  if (!localStorage.getItem('ons')) {
+    factors.forEach(async (factor) => {
+      try {
+        const url = renderOnsUrl(factor);
+        const data = await fetchOnsData(url);
+        const { observations } = data;
+        let wellbeing = observations.map(({ observation }) => observation);
+        let geography = observations.map(
+          (a) => a.dimensions['Geography'].label
+        );
+        let result = {};
+        geography.forEach((key, i) => (result[key] = wellbeing[i]));
+        const filtered = Object.keys(result)
+          .filter((key) => cities.includes(key))
+          .reduce((obj, key) => {
+            obj[key] = result[key];
+            return obj;
+          }, {});
+        factorsFiltered.push(filtered);
+        // Object created
+        let obj = {};
 
-      // Using loop to insert key to value in object
-      for (let i = 0; i < factors.length; i++) {
-        obj[factors[i]] = factorsFiltered[i];
+        // Using loop to insert key to value in object
+        for (let i = 0; i < factors.length; i++) {
+          obj[factors[i]] = factorsFiltered[i];
+        }
+        localStorage.setItem('ons', JSON.stringify(obj));
+      } catch (error) {
+        console.log(error);
       }
-      localStorage.setItem('ons', JSON.stringify(obj));
-    } catch (error) {
-      console.log(error);
-    }
-  });
+    });
+  }
 });
 
 contactBtn.addEventListener('click', function () {
