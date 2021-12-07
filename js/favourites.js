acf11930a989942983e8ee7f7f0cbadcbb5bf1b3
jordi <jordi.lanau@yahoo.com>
2021-12-07 21:16:49 +0000

    Add image to favourites page to display when there are no jobs to display

--- js/favourites.js
+++ js/favourites.js
@@ -1,14 +1,21 @@
 import { renderJobsSearchData } from './utils/renderJobsSearchData.js';
 import './utils/getIndividualJobData.js';
 
-const favouritesBtn = document.querySelector('.favourites');
-
 const favourites = JSON.parse(localStorage.getItem('favourites')) || [];
+const main = document.querySelector('main .column');
 
 window.addEventListener('DOMContentLoaded', function () {
-  renderJobsSearchData(favourites);
-});
-
-favouritesBtn.addEventListener('click', function () {
-  window.location.reload();
+  if (favourites.length === 0) {
+    main.innerHTML = `<figure class="is-flex is-justify-content-center">
+                        <img class="no-favourites" src="./media/img/no-favourites.jpg" alt="no favourites" />
+                      </figure>
+                      <h2 class="title has-text-centered">
+                        You have not favourited any jobs
+                      </h2>
+                      <button class="button is-link is-pulled-right">
+                        <a class="has-text-white" href="/index.html">Go back</a>
+                      </button>`;
+  } else {
+    renderJobsSearchData(favourites);
+  }
 });
