21b9da5faa7b8cc7027861873a7fa9fe461c25a2
jordi <jordi.lanau@yahoo.com>
2021-12-07 21:21:46 +0000

    Add attribution

--- js/favourites.js
+++ js/favourites.js
@@ -6,8 +6,11 @@ const main = document.querySelector('main .column');
 
 window.addEventListener('DOMContentLoaded', function () {
   if (favourites.length === 0) {
-    main.innerHTML = `<figure class="is-flex is-justify-content-center">
+    main.innerHTML = `<figure class="is-flex is-flex-direction-column is-align-items-center">
                         <img class="no-favourites" src="./media/img/no-favourites.jpg" alt="no favourites" />
+                        <figcaption class="attribution">
+                          <a href="https://www.freepik.com/vectors/data">Data vector created by stories - www.freepik.com</a>
+                        </figcaption>
                       </figure>
                       <h2 class="title has-text-centered">
                         You have not favourited any jobs
