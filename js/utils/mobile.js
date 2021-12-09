b0bd8c3c03bb652bc534e7d624cf2a334843950f
jordi <jordi.lanau@yahoo.com>
2021-12-09 13:57:21 +0000

    Remove mobile module

--- js/utils/mobile.js
+++
@@ -1,7 +0,0 @@
-// mobile menu
-const burgerIcon = document.querySelector('.navbar-burger');
-const navbarMenu = document.querySelector('.navbar-menu');
-
-burgerIcon.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active');
-});
