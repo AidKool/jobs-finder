bec56de302874974895b599252609a5a2aaca5b7
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2021-12-10 11:11:15 +0000

    delete unnecessary console logs

--- js/utils/leaflet.js
+++ js/utils/leaflet.js
@@ -7,6 +7,3 @@ export const tileLayer = L.tileLayer(
       '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
   }
 ).addTo(map);
-
-// export let marker_man = L.marker([53.483959, -2.244644]).addTo(map);
-// export let marker_ldn = L.marker([51.5, -0.09]).addTo(map);
