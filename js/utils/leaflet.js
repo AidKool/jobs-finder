8c2cd4785a44b993ac52f9cd256e7806f22f0d7b
jordi <jordi.lanau@yahoo.com>
2021-12-10 21:21:47 +0000

    Replace MapTiler API key

--- js/utils/leaflet.js
+++ js/utils/leaflet.js
@@ -1,7 +1,7 @@
 export const map = L.map('map').setView([53.483959, -2.244644], 6);
 
 export const tileLayer = L.tileLayer(
-  'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=asHo0upHjsCi8cGPV9nd',
+  'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=vXHoLejq0rBnYbqJIdKe',
   {
     attribution:
       '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
