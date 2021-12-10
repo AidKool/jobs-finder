2684e7d92a75c1f1c2adfcd9ed6bd51165d348c3
jordi <jordi.lanau@yahoo.com>
2021-12-10 21:41:32 +0000

    Fix import in renderPaginationURL module

--- js/utils/renderPaginationURL.js
+++ js/utils/renderPaginationURL.js
@@ -1,4 +1,4 @@
-export { VALUES_PER_PAGE } from './constants.js';
+import { VALUES_PER_PAGE } from './constants.js';
 
 export function renderPaginationURL(url, currentPage) {
   const resultsToSkip = (currentPage - 1) * VALUES_PER_PAGE;
